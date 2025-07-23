b98c2059fd6d9930bc63cf86255c08b80fd6f349
Rorza <85464674+SlothieBoi@users.noreply.github.com>
2025-07-23 18:35:49 +1000

    Initial commit: Has functionality to load .gbl files from /models directory

---
+++ viewer.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+loader.load(
+    'models/shiba.glb',
+
+    function (glb) {
+        scene.add(glb.scene);
+    },
+    function ( xhr ) {
+
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+    },
+    // called when loading has errors
+    function ( error ) {
+
+        console.log('An error happened');
+    }
+)
+
+
+camera.position.z=2;
+
+function animate() {
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
